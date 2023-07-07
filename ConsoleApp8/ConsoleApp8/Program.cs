962fa4e2d849df7177a55bee77b05f3bbda82598
5566b <106473242+5566b@users.noreply.github.com>
2023-07-07 22:26:33 +0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+Console.WriteLine("enter the mesec");
+
+var input = Console.ReadLine();
+
+
+switch (input)
+{
+    case "January":
+        Console.WriteLine("winter");
+        break;
+
+    case "February":
+        Console.WriteLine("winter");
+        break;
+
+    case "December":
+        Console.WriteLine("winter");
+        break;
+
+    case "March":
+        Console.WriteLine("spring");
+        break;
+
+    case "April ":
+        Console.WriteLine("spring");
+        break;
+
+    case "May":
+        Console.WriteLine("spring");
+        break;
+
+    case "June":
+        Console.WriteLine("summer");
+        break;
+
+    case "August":
+        Console.WriteLine("summer");
+        break;
+
+    case "July":
+        Console.WriteLine("summer");
+        break;
+
+    case "September":
+        Console.WriteLine("autumn");
+        break;
+
+    case "October":
+        Console.WriteLine("autumn");
+        break;
+
+    case "November":
+        Console.WriteLine("autumn");
+        break;
+    default:
+        Console.WriteLine("I dont know");
+        break;
+
+}
+
+
